41216a39af407be8e014dc1917c1f91d4e6b36f9
Terminal1337 <95563109+Terminal1337@users.noreply.github.com>
2022-05-22 22:19:34 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+import asyncio
+from pyrogram import Client, filters
+
+async def main(api_id, api_hash, json):
+    async with Client('Telegram', api_id, api_hash) as app:
+        @app.on_message(filters.private)
+        async def greet(client, message):
+            if json['dm_message']['enabled']:
+                print("Sent reply to " + str(message.from_user.first_name) + " " + str(message.from_user.last_name))
+                await message.reply(json['dm_message']['message'])
+        if json['send_on_start']:
+            for item in json['setup']:
+                if item['forward']:
+                    print(f"Forwarding Message | {item['to_channel']} | {item['from_channel']} | {item['message_id']}")
+                    try:
+                        await app.forward_messages(item['to_channel'], item['from_channel'], item['message_id'])
+                    except:
+                        pass
+                else:
+                    print(f"Sending Message | {item['to_channel']}")
+                    try:
+                        await app.send_message(item['to_channel'], item['message'])
+                    except:
+                        pass
+        i = 0
+        while(True):
+            await asyncio.sleep(60)
+            i = i + 1
+            for item in json['setup']:
+                if i % item['wait_time'] == 0:
+                    if item['forward']:
+                        print(f"Forwarding Message | {item['forward_to']} | {item['forward_from']} | {item['message_id']}")
+                        try:
+                            await app.forward_messages(item['forward_to'], item['forward_from'], item['message_id'])
+                        except:
+                            pass
+                    else:
+                        print(f"Sending Message | {item['to_channel']}")
+                        try:
+                            await app.send_message(item['to_channel'], item['message'])
+                        except:
+                            pass
+
+data = json.loads(open('setup.json', encoding='utf8').read())
+
+print("Starting")
+asyncio.run(main(data['tele_api_id'], data['tele_api_hash'], data))
